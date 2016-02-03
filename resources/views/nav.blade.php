d10211dbc1b7f40de12fe1691e40f2e48a60e27a
Jethro Ordaneza <jet.oneza@gmail.com>
2016-02-04 00:54:51 +0800

    Nav adjustments

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,39 +1,39 @@
 <style type="text/css" media="screen">
-  
-  .nav {
-  	padding: 25px 220px;
-  	background-color: black;
-  }
- 
-  .menu {
-    margin: 0;
-    display: inline-block;
-    float: right;
-  }
 
-  .item {
-    margin-left: 50px;
-    display: inline-block;
-  }
+    .nav {
+        padding: 25px 220px;
+        background-color: black;
+    }
 
-   a {
-    font-size: 14px;
-    cursor: pointer;
-    text-decoration: none;
-    color: #cccccc;
-   }
-   
-  a:hover {
-    color: #cccccc;
-    padding-bottom: 3px;
-    border-bottom: 1px solid rgba(255,255,255, 0.5);
-   }
+    .menu {
+        margin: 0;
+        display: inline-block;
+        float: right;
+    }
 
-  a:after {
-    content: "";
-    display: table;
-    clear: both;
-  }
+    .item {
+        margin-left: 50px;
+        display: inline-block;
+    }
+
+    li.item a {
+        font-size: 14px;
+        cursor: pointer;
+        text-decoration: none;
+        color: #cccccc;
+    }
+
+    li.item a:hover {
+        color: #cccccc;
+        padding-bottom: 3px;
+        border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+    }
+
+    a:after {
+        content: "";
+        display: table;
+        clear: both;
+    }
 
 </style>
 
@@ -48,23 +48,23 @@
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
 <script type="text/javascript">
-$(function() {
+    $(function () {
 
-    var $sidebar   = $("#nav"), 
-        $window    = $(window),
-        offset     = $sidebar.offset(),
-        topPadding = 15;
+        var $sidebar = $("#nav"),
+                $window = $(window),
+                offset = $sidebar.offset(),
+                topPadding = 15;
 
-    $window.scroll(function() {
-        if ($window.scrollTop() > offset.top) {
-            $sidebar.stop().animate({
-                marginTop: $window.scrollTop() - offset.top + topPadding
-            });
-        } else {
-            $sidebar.stop().animate({
-                marginTop: 0
-            });
-        }
+        $window.scroll(function () {
+            if ($window.scrollTop() > offset.top) {
+                $sidebar.stop().animate({
+                    marginTop: $window.scrollTop() - offset.top + topPadding
+                });
+            } else {
+                $sidebar.stop().animate({
+                    marginTop: 0
+                });
+            }
+        });
+
     });
-    
-});
