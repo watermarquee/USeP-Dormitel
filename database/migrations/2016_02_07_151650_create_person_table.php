13db822ff170301414fd6bb6de9747c8b0d60d42
Jethro Ordaneza <jet.oneza@gmail.com>
2016-02-07 23:40:59 +0800

    Person migration

---
+++ database/migrations/2016_02_07_151650_create_person_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonTable extends Migration
+{
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('persons', function (Blueprint $table) {
+      $table->increments('id');
+      $table->string('first_name');
+      $table->string('last_name');
+      $table->string('address');
+      $table->string('email');
+      $table->string('phone');
+      $table->string('country');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('persons');
+  }
+
+}
