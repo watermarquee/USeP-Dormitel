25e3c63e3934113f2a2df62ea75d7c2ad9a0e6cb
Mark Lawrence <Mark Lawrence>
2016-02-23 01:58:00 +0800

    Changed price type to double.

--- database/migrations/2016_02_22_173945_add_status_and_price_to_rooms_table.php
+++ database/migrations/2016_02_22_173945_add_status_and_price_to_rooms_table.php
@@ -5,31 +5,19 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddStatusAndPriceToRoomsTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
 	public function up()
 	{
 		Schema::table('rooms', function(Blueprint $table)
 		{
-			//
 			$table->string('availability');
-			$table->integer('price');
+			$table->double('price');
 		});
 	}
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
 	public function down()
 	{
 		Schema::table('rooms', function(Blueprint $table)
 		{
-			//
 			$table->dropColumn('availability');
 			$table->dropColumn('price');
 		});
